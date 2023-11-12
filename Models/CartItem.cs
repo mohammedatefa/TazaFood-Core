184cdec187047a11ff10d2122771aa50d94f01e5
mohammedatefa <mohamed.atef.abdelsbor.com>
2023-11-12 15:14:28 +0200

    create users account controller and handle the end points of Login And Registeration using Identity &Token Services

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -11,8 +11,8 @@ namespace TazaFood_Core.Models
         public string  Name { get; set; }
         public string Description { get; set; }
         public int Price { get; set; }
-        public int Rate { get; set; }
-        public string category { get; set; }
-        public int Quantity { get; set; }
+        public string Category { get; set; }
+        public string ImageUrl { get; set; }
+        public int Quantity { get; set; } = 1;
     }
 }
