f37ccbde5a42e96885d384519bc06be720be5106
mohammedatefa <mohamed.atef.abdelsbor.com>
2023-11-16 20:37:23 +0200

    add last update

--- Services/IPaymentService.cs
+++ Services/IPaymentService.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TazaFood_Core.Models;
+using TazaFood_Core.Models.Order_Aggregate;
 
 namespace TazaFood_Core.Services
 {
     public interface IPaymentService
     {
         Task<UserCart> CreateOrUpdatePaymentIntent(string cartId);
+        Task<Order> UpdatePaymentIntentWithSucceededOrFaild(string paymentIntent, bool flag);
     }
 }
