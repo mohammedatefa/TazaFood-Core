32bf4fa8b6657142e59e5614d0d43c3aec09eb68
alisayed2 <alisayed.cs1@gmail.com>
2023-11-16 14:58:01 +0200

    Account Controller (Update User , Delete User) End Points

---
+++ Models/Review.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TazaFood_Core.Models
+{
+    public class Review : BaseModel
+    {
+        public string Content { get; set; }
+    }
+}
